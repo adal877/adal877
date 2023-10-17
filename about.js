f043d519902b7f0a50db79dd45e487238301bb2c
Adalberto Junior <59984156+adal877@users.noreply.github.com>
2023-10-17 12:33:37 -0300

    Create about.js

---
+++ about.js
@@ -0,0 +1,60 @@
+export const about_me = {
+    first_name: "Adalberto",
+    last_name: "Junior",
+    nick_name: "Adal",
+    social: new Map([
+        ["github", "https://github.com/adal877"],
+        ["linkedin", "https://linkedin.com/in/adalberto-junior-7050a1202"]
+    ]),
+    skills: {
+        soft: [
+            "Communication",
+            "Teamwork",
+            "Adaptability",
+            "Problem-solving",
+            "Time management",
+            "Leadership",
+            "Creativity",
+            "Interpersonal skills",
+            "Emotional intelligence",
+            "Conflict resolution",
+            "Active listening",
+            "Critical thinking",
+            "Stress management",
+            "Negotiation",
+            "Networking",
+            "Decision-making",
+            "Empathy",
+            "Open-mindedness",
+            "Conflict management",
+            "Self-motivation",
+            "Organization",
+            "Flexibility",
+            "Positivity",
+            "Conflict resolution",
+            "Customer service",
+            "Conflict resolution",
+            "Conflict management"
+        ],
+        hard: {
+            languages: ["Java", "Spring Boot", "SalesForce", "React"],
+            linux: ["Debian linux", "Void linux", "Linux Server"],
+            idioms: new Map([
+                ["English",
+                    {
+                        writing: "####",
+                        speaking: "###",
+                        listening: "####"
+                    }
+                ],
+                ["Russian",
+                    {
+                        writing: "#",
+                        speaking: "#",
+                        listening: "#"
+                    }
+                ]
+            ])
+        }
+    }
+}
